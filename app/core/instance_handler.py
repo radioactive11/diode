bcb7d42cfdf70edd13501186434c51eea6b92331
radioactive11 <roy.arijit2001@gmail.com>
2022-06-28 02:07:10 +0530

    :mute: `SSH Key FIle` Removed saving ssk key as txt file

--- app/core/instance_handler.py
+++ app/core/instance_handler.py
@@ -6,19 +6,17 @@ from dotenv import load_dotenv
 
 from app.schema import instance
 
+PUBLIC_KEY = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCBqtVjKq3VjnWpcerUYCmUrLPFBXyE6M7kxulQ2qabdXgvu3cu9lRxgZS7K/8lJMhS39EgFtdoD+AowYD73g0Oj0XTn3rNhr9LIUxWD0rZuVDma9tzwUEwWdRMeLZgnjGA6AllprHb+EF0K2F9bXuOuT9N3UpN9j6Gs2LQ+xCSvWElZrugpfQvlBq2MdMIkQy2s3GZZG9Rdpf9RhqkjRZg23iWL7fHB4kdcWI6KZsWSJuE/H63LtHheFGqMmQtFFBiqtQGVLkqj3EH/iQih9348oujs0pVVXMcm+Ji46yag5AVGu1JoRLf14Q9UxlYc/cDXcLXHNfhg8BN9oH3jGtn"
+
 
 def create_instance(details: instance.CreateInstance, client: LinodeClient) -> Dict:
     new_linode, password = client.linode.instance_create(
-        details.instance_type, details.region, details.image
+        details.instance_type, details.region, details.image, authorized_keys=PUBLIC_KEY
     )
 
     ip4_addr = new_linode.ipv4[0]
     _id = new_linode.id
 
-    with open(f"./ssh_{_id}_key.txt", "w") as f:
-        f.write(password)
-
-    f.close()
 
     return {
         "id": _id,
